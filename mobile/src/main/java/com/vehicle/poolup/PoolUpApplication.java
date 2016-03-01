44823a855b4f45656a58763f70a83ed917591402
mohitagarwal <mohit.tibrewal@flipkart.com>
2016-03-01 22:02:22 +0530

    adding application and only using gcm play service

---
+++ mobile/src/main/java/com/vehicle/poolup/PoolUpApplication.java
@@ -0,0 +1,15 @@
+package com.vehicle.poolup;
+
+import android.app.Application;
+
+/**
+ * Created by mohit.tibrewal on 01/03/16.
+ */
+public class PoolUpApplication extends Application {
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+    }
+}
